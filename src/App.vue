2a4fe6087bf0f9649234b044c93e24aa5bdf62fe
jlcudris <weiselcudris@hotmail.com>
2023-04-17 05:12:50 -0500

    se modifica el navbargit

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
+          <router-link class="nav-link active" aria-current="page" href="#" to="/">Inicio</router-link>
+        </li>
+        <li class="nav-item">
           <router-link class="nav-link active" aria-current="page" href="#" to="/list">Consultar</router-link>
         </li>
         <li class="nav-item">
